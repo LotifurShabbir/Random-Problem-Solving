59eff745226b1f844ef2c3a780376dab16654662
Junior Developer <lotifurshabbir54@gmail.com>
2024-01-03 18:13:17 +0600

    cpp solution added

--- Daily Leetcode/2125_Number_of_Laser_Beams_in_a_Bank.py
+++ Daily Leetcode/2125_Number_of_Laser_Beams_in_a_Bank.py
@@ -11,3 +11,23 @@ class Solution:
             prev_one = one_count
 
         return ans
+#-----------------------------------
+    # cpp solution
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int ans = 0;
+        int prevOne = 0;
+
+        for (auto s : bank) {
+            int oneCount = count(s.begin(), s.end(), '1');
+            if (oneCount == 0) {
+                continue;
+            }
+            ans += oneCount * prevOne;
+            prevOne = oneCount;
+        }
+
+        return ans;
+    }
+};
